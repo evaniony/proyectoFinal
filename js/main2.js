e8f7716a7afae7f3556d3cd7e2afe21e048589be
Eva Quinteros Gonzalez <ebi.quinterosgonzalez@gmail.com>
2022-08-31 12:36:47 -0700

    toastify + ajuste

--- js/main2.js
+++ js/main2.js
@@ -1,7 +1,7 @@
 const section = document.querySelector(".section-productos");
 const navCarrito = document.querySelector(".carrito");
 
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
 const productos = [
     {
@@ -83,18 +83,29 @@ function buttonFn(){
     productos.forEach((prod) => {
         document.querySelector(`#btn-agregar${prod.art}`).addEventListener("click", ()=>{
             addCarrito(prod);
+            Toastify({
+                text: "Agregaste un producto!",
+                duration: 2000,
+                gravity: 'top',
+                position: 'right',
+                style: {
+                    background: 'green'
+                }
+            }).showToast();
         })
     })
 }
+
+
 
 function addCarrito(prod){
     let exist = carrito.some((prodSome) => prodSome.art === prod.art);
     if(exist === false){
         prod.cantidad = 1;
-        carrito.push(prod);
+        carrito.push(prod)
     }else{
         let find = carrito.find((prodFind) => prodFind.art === prod.art);
-        find.cantidad++ 
+        find.cantidad++
     }
     renderCarrito()
 }
@@ -103,9 +114,9 @@ function addCarrito(prod){
 
 function renderCarrito(){
     navCarrito.innerHTML = "";
-    pay();
+    pay()
     carrito.forEach((prod) => {
-        navCarrito.innerHTML += `<div style = "background-color: pink">
+        navCarrito.innerHTML += `<div style = "background-color: red">
                                  <h3>${prod.nombre}</h3>
                                  <p>$${prod.importe}</p>
                                  <p>Cantidad: ${prod.cantidad}</p>
@@ -117,28 +128,34 @@ function renderCarrito(){
 }
 
 function deleteProd(){
+    //debugger
     carrito.forEach((prod) => {
         document.querySelector(`#btn-borrar${prod.art}`).addEventListener("click", ()=>{
-            carrito = carrito.filter(prodFilter => prodFilter.art !== prod.art);
+            carrito = carrito.filter(item => item.cantidad !== 1);
+            if(prod.cantidad > 1){
+                prod.cantidad--
+            }
+            Toastify({
+                text: "Eliminaste un producto!",
+                duration: 2000,
+                gravity: 'top',
+                position: 'left',
+                style: {
+                    background: 'red'
+                }
+            }).showToast();
             renderCarrito();
+            
         })
     })
 }
 
 function pay(){
-    let total = carrito.reduce((acc, prod) => acc + prod.importe, 0);
+    let total = carrito.reduce((acc, prod) => acc + (prod.importe * prod.cantidad), 0);
     navCarrito.innerHTML += `<h1>Total: $${total}</h1>`;
-
-    let addProd = carrito.some((prodSome) => prodSome.importe === producto.importe);
-    if(addProd === false){
-        prod.cantidad = 1;
-        carrito.push(prod);
-        return cantidad;
-    }else{
-        let sumarProd = carrito.find((prodFind) => prodFind.importe === producto.importe);
-         sumarProd.importe * prod.cantidad;
-    }
 }
+
 
 cards();
 renderCarrito();
+
