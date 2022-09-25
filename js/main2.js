0f7b72a9ce1d611aa7de19b856f3a0ab59404408
Eva Quinteros Gonzalez <ebi.quinterosgonzalez@gmail.com>
2022-09-24 22:25:40 -0700

    Tienda de accesorios: ROMA

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,7 @@
 const section = document.querySelector(".section-productos");
 const navCarrito = document.querySelector(".carrito");
+const carritoTotal = document.querySelector(".total-carrito")
+const form = document.querySelector(".form-buy");
 let productos = [];
 let carrito = JSON.parse(localStorage.getItem("carrito")) || []
 
@@ -16,11 +18,11 @@ contenido();
 
 function cards(){
     productos.forEach((prod) => {
-        section.innerHTML += `<div class="card">
+        section.innerHTML += `<div class="card rounded-md p-3 grid justify-items ...">
                               <img class="images" src="${prod.img}">
                               <h3>${prod.nombre}</h3>
                               <p>$${prod.importe}</p>
-                              <button id="btn-agregar${prod.art}">Agregar</button>
+                              <button id="btn-agregar${prod.art}"><i class="fa-solid fa-cart-shopping"></i></button>
                              </div>`
     })
     buttonFn();
@@ -30,17 +32,33 @@ function buttonFn(){
     productos.forEach((prod) => {
         document.querySelector(`#btn-agregar${prod.art}`).addEventListener("click", ()=>{
             addCarrito(prod);
+            mostrar();
             Toastify({
                 text: "Agregaste un producto!",
                 duration: 2000,
                 gravity: 'top',
                 position: 'right',
                 style: {
-                    background: 'green'
+                    background: 'black',
+                    color: 'white'
                 }
             }).showToast();
         })
     })
+}
+
+function minus(){
+    document.querySelector(".img-closed").addEventListener("click", ()=>{
+        ocultar();
+    })
+}
+
+function ocultar() {
+    document.querySelector(".carrito").style.display = "none";
+}
+
+ function mostrar(){
+    document.querySelector(".carrito").style.display = "block";
 }
 
 
@@ -57,22 +75,73 @@ function addCarrito(prod){
     renderCarrito()
 }
 
-
-
 function renderCarrito(){
-    navCarrito.innerHTML = "";
+    navCarrito.innerHTML = ""
     pay()
     carrito.forEach((prod) => {
-        navCarrito.innerHTML += `<div style = "background-color: red">
-                                 <img class="images aside" src="${prod.img}">
+        navCarrito.innerHTML += `<div class="card-in">
+                                 <img class="images-aside" src="${prod.img}">
                                  <h3>${prod.nombre}</h3>
                                  <p>$${prod.importe}</p>
                                  <p>Cantidad: ${prod.cantidad}</p>
-                                 <button class="btnCarrito" id="btn-borrar${prod.art}">Borrar</button>
+                                 <button class="btnCarrito" id="btn-borrar${prod.art}">delete</button>
                                 </div>`
     })
+    getBuy();
+    if (carrito == 0){ ocultar();}
     localStorage.setItem("carrito", JSON.stringify(carrito));
     deleteProd();
+    minus();
+    clickBtn();
+
+}
+
+function getBuy(){
+    let btn = document.createElement("button");
+    btn.className = "btn-buy";
+    btn.innerText = "REALIZAR COMPRA";
+    navCarrito.append(btn);
+    
+}
+
+function clickBtn(){
+    document.querySelector(".btn-buy").addEventListener("click", ()=>{
+        formBtn(),
+        mostrarForm()
+    })
+}
+
+function formBtn(){;
+    form.innerHTML = `<h2>Completa el siguiente formulario para continuar tu compra.</h2>
+                      <p>Una vez realizado el pago, enviaremos tus productos. De lo contrario, la compra se cancela.</p>
+                      <input type="text" value="John Doe">
+                      <input type="email" value="johnDoe@example.com">
+                      <p>Recibirás un mail para efectuar tu compra.</p>
+                      <button class="btn-form">COMPRAR</button>`
+    ocultarForm();
+    finishBuy();
+}
+
+function ocultarForm() {
+    document.querySelector(".form-buy").style.display = "none";
+}
+
+ function mostrarForm(){
+    document.querySelector(".form-buy").style.display = "block";
+}
+
+function finishBuy(){
+    document.querySelector(".btn-form").addEventListener("click", ()=>{
+        Swal.fire(
+            'COMPRA EXITOSA!',
+            'Muchas gracias!',
+            'success',
+          )
+          ocultar();
+          ocultarForm();
+          carrito = [];
+          navCarrito.innerHTML = "";
+    })
 }
 
 function deleteProd(){
@@ -83,27 +152,48 @@ function deleteProd(){
             if(prod.cantidad > 1){
                 prod.cantidad--
             }
-            Toastify({
-                text: "Eliminaste un producto!",
-                duration: 2000,
-                gravity: 'top',
-                position: 'left',
-                style: {
-                    background: 'red'
-                }
-            }).showToast();
             renderCarrito();
-            
         })
     })
 }
 
 function pay(){
     let total = carrito.reduce((acc, prod) => acc + (prod.importe * prod.cantidad), 0);
-    navCarrito.innerHTML += `<h1>Total: $${total}</h1>`;
+    navCarrito.innerHTML = `<img class="img-closed"src="./svg/minus.svg">
+                            <h1 class= "total">Total: $${total}</h1>`;
+        document.querySelector(".img-closed").addEventListener("click", ()=>{
+        if (carrito == 0){
+            ocultar();
+        }
+    })
+    if(carrito == 0){
+        Toastify({
+            text: "Tu carrito está vacío!",
+            duration: 2000,
+            gravity: 'top',
+            position: 'right',
+            style: {
+                background: 'white',
+                color: 'black'
+            }
+        }).showToast();
+    }
 }
 
+function agregando(){
+    document.querySelector(".total-carrito").addEventListener("click", ()=>{
+        mostrar();
+    })
+}
+
+/* function buttonFinish(){
+    let btn = document.querySelector(".carrito");
+    btn.innerHTML = `<button class="btn-finish">Realizar compra</button>`
+    
+} */
 
 cards();
 renderCarrito();
-
+agregando();
+minus();
+formBtn();
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./style/style.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
